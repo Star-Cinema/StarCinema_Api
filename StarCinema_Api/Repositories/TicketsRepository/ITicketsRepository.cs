d24e42c25a471d436228bbb908883b99c179008b
anhnguyen <anhn32616@gmail.com>
2023-05-24 12:13:19 +0700

    add comment code

--- StarCinema_Api/Repositories/TicketsRepository/ITicketsRepository.cs
+++ StarCinema_Api/Repositories/TicketsRepository/ITicketsRepository.cs
@@ -2,8 +2,14 @@
 
 namespace StarCinema_Api.Repositories.TicketsRepository
 {
+    /*
+        Account : AnhNT282
+        Description : Interface repository for entity ticket
+        Date created : 2023/05/05
+    */
     public interface ITicketsRepository : IBaseRepository<Tickets>
     {
+        // Get id of last ticket AnhNT282
         Task<int> GetLastIDTicket();
     }
 }
--- StarCinema_Api/Services/PaymentService/IPaymentService.cs
+++ StarCinema_Api/Services/PaymentService/IPaymentService.cs
@@ -3,11 +3,20 @@ using StarCinema_Api.DTOs;
 
 namespace StarCinema_Api.Services.PaymentService
 {
+    /*
+        Account : AnhNT282
+        Description : Interface service for entity payment
+        Date created : 2023/05/19
+    */
     public interface IPaymentService
     {
+        // Get all payment AnhNT282
         Task<ResponseDTO> GetPaymentListAsync();
+        // Create payment AnhNT282
         Task<ResponseDTO> CreatePaymentAsync(Payment payment);
+        // Delete payment AnhNT282
         Task<ResponseDTO> DeletePayment(long id);
-        Task<ResponseDTO> PaymentGetPaymentById(long id);
+        // Get payment by id AnhNT282
+        Task<ResponseDTO> GetPaymentById(long id);
     }
 }
